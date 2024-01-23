490e87ba9d64610de7d78d63919e341800223bd3
Harish Kumar P <harizverse@gmail.com>
2024-01-23 19:38:53 +0530

    Add files via upload

---
+++ Day 21 to 30/Day 22/paddle.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+pad = Turtle()
+
+
+class Paddle:
+    def __init__(self,):
+        pad.shape("rectangle")
+        pad.color("white")
+        pad.shapesize(20,100)
+        pad.goto(350,0)
+
+
+
